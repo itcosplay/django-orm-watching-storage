d5274fbe1fec2e2c43c3b5b7975ee4e65b8cfa8e
itcosplay <azure_dream@mail.ru>
2021-11-01 15:22:24 +0300

    Added .env, corrected  sittings.py according to .env, replaced main.py to mangage.py

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,30 @@
 import os
 
+from dotenv import load_dotenv
+from environs import Env
+
+
+load_dotenv()
+
+env = Env()
+env.read_env()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ROOT_URLCONF = "project.urls"
 
